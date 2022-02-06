1fc3b70bb36364f624f1adcfa7e9acc4cd058d79
pedrobipede <pedroivomartinsdevasconcelos@gmail.com>
2022-02-06 16:12:51 -0300

    remove unused import

--- src/main/java/br/com/softplan/pessoas/control/PessoaController.java
+++ src/main/java/br/com/softplan/pessoas/control/PessoaController.java
@@ -6,7 +6,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.softplan.pessoas.dto.PessoaDTO;
